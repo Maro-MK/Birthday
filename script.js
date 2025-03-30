fe7a4bacee3a303e4fa918e0d4b4ce78d07ef531
Marwan Mohamed Khalifa <mrawan10152011@gmail.com>
2025-03-30 17:38:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ function check_data(){
     document.getElementsByTagName("section")[0].style.display = "block";
 
     const name = document.getElementById("name").value;
-    const spans = document.getElementsByTagName("span");
+    const user_name = document.getElementById("user");
     const audio = document.getElementsByTagName("audio")[0];
-
-    for(let i = 0; i < spans.length; i++){
-        spans[i].textContent = name;
-    }
+    
+    user_name.textContent = name;
     document.title = name + " - Happy Birthday !";
     audio.play();
 }
